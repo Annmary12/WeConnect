fb147bfbc2773967fc4ad585026fc4d51365a0b8
Annmary12 <annmaryamaka@yahoo.com>
2018-03-05 11:21:42 +0100

    Environmental Setup

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import winston from 'winston';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to wecconect app...');
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  winston.info(`Connected on port ${port}`);
+});
+
